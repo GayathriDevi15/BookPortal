815996d998d33a9247225c73296b7038941c0e88
Sravanikonanki <159441208+Sravanikonanki@users.noreply.github.com>
2024-03-16 20:39:34 +0530

    Delete bookpartnerportal/src/main/java/com/bookpartnerportal/bookpartnerportal/publisherexception directory

--- bookpartnerportal/src/main/java/com/bookpartnerportal/bookpartnerportal/publisherexception/TitleswithPubdateNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.bookpartnerportal.bookpartnerportal.publisherexception;
-
-public class TitleswithPubdateNotFoundException extends RuntimeException {
-  public TitleswithPubdateNotFoundException(String message) {
-	  super(message);
-  }
-}
